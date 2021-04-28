a3a9004e15738b13639663bb2298cd2cd61cd994
Neibue <devwithmax@outlook.com>
2021-04-28 09:48:04 +0200

    mappind endpoints to controllers instead of routes

--- MyLittleCrm/MyLittleCrm_API/Startup.cs
+++ MyLittleCrm/MyLittleCrm_API/Startup.cs
@@ -42,10 +42,7 @@ namespace MyLittleCrm_API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
