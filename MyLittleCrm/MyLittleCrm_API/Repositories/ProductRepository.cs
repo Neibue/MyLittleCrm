3ecb383edceb1f7aa4c824dee0b1338246f88cd9
Neibue <devwithmax@outlook.com>
2021-04-28 16:08:58 +0200

    Adding product and Quote Repository

---
+++ MyLittleCrm/MyLittleCrm_API/Repositories/ProductRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyLittleCrmApi.Models;
+using MyLittleCrm_API.Contracts;
+using MyLittleCrm_API.Data;
+
+namespace MyLittleCrm_API.Repositories
+{
+    public class ProductRepository : IBaseRepository<Product>
+    {
+        private readonly MyLittleCrmContext _dbContext;
+        public ProductRepository(MyLittleCrmContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> Create(Product entity)
+        {
+            await _dbContext.Products.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Product entity)
+        {
+            _dbContext.Products.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<bool> DeleteAll(IList<Product> entity)
+        {
+            bool result = false;
+            foreach (var p in entity)
+            {
+                result = await Delete(p);
+            }
+            return result;
+        }
+
+        public async Task<IList<Product>> FindAll()
+        {
+            return await _dbContext.Products
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Product> FindById(int id)
+        {
+            return await _dbContext.Products.FindAsync(id);
+        }
+
+        public async Task<bool> IsExists(int id)
+        {
+            return await _dbContext.Products.AnyAsync(p => p.ProductID == id);
+        }
+
+        public async Task<bool> Save()
+        {
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Update(Product entity)
+        {
+            _dbContext.Products.Update(entity);
+            return await Save();
+        }
+    }
+}
---
+++ MyLittleCrm/MyLittleCrm_API/Repositories/QuoteRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyLittleCrmApi.Models;
+using MyLittleCrm_API.Contracts;
+using MyLittleCrm_API.Data;
+
+
+namespace MyLittleCrm_API.Repositories
+{
+    public class QuoteRepository : IBaseRepository<Quote>
+    {
+        private readonly MyLittleCrmContext _dbContext;
+        public QuoteRepository(MyLittleCrmContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Create(Quote entity)
+        {
+            await _dbContext.Quotes.AddAsync(entity);
+            return await Save();
+        }
+
+        public async Task<bool> Delete(Quote entity)
+        {
+            _dbContext.Quotes.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<bool> DeleteAll(IList<Quote> entity)
+        {
+            bool result = false;
+            foreach (var q in entity)
+            {
+                result = await Delete(q);
+            }
+            return result;
+        }
+
+        public async Task<IList<Quote>> FindAll()
+        {
+            return await _dbContext.Quotes
+                .Include(q => q.QuoteLines)
+                    .ThenInclude(ql => ql.Product)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Quote> FindById(int id)
+        {
+            return await _dbContext.Quotes
+                .Include(q => q.QuoteLines)
+                    .ThenInclude(ql => ql.Product)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(q => q.QuoteID == id);
+        }
+
+        public async Task<bool> IsExists(int id)
+        {
+            return await _dbContext.Quotes.AnyAsync(q => q.QuoteID == id);
+        }
+
+        public async Task<bool> Save()
+        {
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Update(Quote entity)
+        {
+            _dbContext.Quotes.Update(entity);
+            return await Save();
+        }
+    }
+}
