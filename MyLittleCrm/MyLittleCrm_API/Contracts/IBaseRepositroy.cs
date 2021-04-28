ededcc1e343b0857f5353988fd1a5f1de8283780
Neibue <devwithmax@outlook.com>
2021-04-28 14:47:49 +0200

    Adding BaseRepository Interface

---
+++ MyLittleCrm/MyLittleCrm_API/Contracts/IBaseRepositroy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLittleCrm_API.Contracts
+{
+    interface IBaseRepositroy<T> where T : class
+    {
+        Task<IList<T>> FindAll();
+        Task<T> FindById(int id);
+        Task<bool> IsExists(int id);
+        Task<bool> Create(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+        Task<bool> DeleteAll(IList<T> entity);
+        Task<bool> Save();
+    }
+}
